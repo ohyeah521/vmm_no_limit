fb7941bbac4aad0461948de357ef7d1e761261a6
Ing <wjz304@qq.com>
2025-04-21 18:01:40 +0800

    Refactor code structure for improved readability and maintainability

--- activated.sh
+++ activated.sh
@@ -1,34 +1,167 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#
+# Copyright (C) 2025 ohyeah521 <https://github.com/ohyeah521>
+#
+# This is free software, licensed under the GPLv3 License.
+# See /LICENSE for more information.
+#
 
+WORK_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+install() {
+  _get_files() {
+    local url="${1}" file="${2}"
+    echo "Downloading $(basename "${url}" 2>/dev/null) ..."
+    mkdir -p "$(dirname "${file}" 2>/dev/null)" 2>/dev/null
+    STATUS="$(curl -skL -w "%{http_code}" "${url}" -o "${file}")"
+    STATUS="${STATUS: -3}"
+    if ! echo "200 404" | grep -wq "${STATUS}"; then
+      echo "Error: ${STATUS}, Failed to download ${url} from GitHub."
+      exit 1
+    fi
+  }
+  
+  _process_file() {
+    local file="${1}" dest="${2}" suffix="${3}" mode="${4}"
+    echo "Patch ${dest}"
+    [ ! -f "${file}" ] && {
+      echo "Warning: ${file} not exist, skip."
+      return 1
+    }
+    [ ! -f "${dest}${suffix}" ] && cp -pf "${dest}" "${dest}${suffix}"
+    cp -f "${file}" "${dest}"
+    chown root:root "${dest}"
+    chmod "${mode}" "${dest}"
+  }
 
-# 屏蔽认证服务器
-if grep -q "synosurveillance.synology.com" /etc/hosts; then
-  echo "already blocked license server: synosurveillance.synology.com."
-else
-  echo "add block license server: synosurveillance.synology.com"
-  echo "0.0.0.0 synosurveillance.synology.com" | sudo tee -a /etc/hosts
-fi
+  ISDL=false
+  if [ ! -d "${WORK_PATH}/patch/${VERSION}/${SS_NAME}" ]; then
+    REPO="${REPO:-"ohyeah521/vmm_no_limit"}"
+    BRANCH="${BRANCH:-"main"}"
 
+    # 检查版本是否存在
+    VERURL="https://github.com/${REPO}/tree/${BRANCH}/patch/${VERSION}/${SS_NAME}"
+    STATUS="$(curl -s -m 10 -connect-timeout 10 -w "%{http_code}" "${VERURL}" -o /dev/null 2>/dev/null)"
+    STATUS="${STATUS: -3}"
+    case "${STATUS}" in
+    "200") ;;
+    "403")
+      echo "Error: ${STATUS}, Access forbidden to the package on GitHub."
+      exit 1
+      ;;
+    "404")
+      echo "Error: ${STATUS}, Current version not found patch on GitHub."
+      exit 1
+      ;;
+    *)
+      echo "Error: ${STATUS}, Failed to download package from GitHub."
+      exit 1
+      ;;
+    esac
 
-# 定义处理文件的函数
-process_file() {
-  local dir="$1"
-  local file="$2"
-  local url="$3"
-  local mode="$4"
+    # 获取 patch 文件
+    URL_FIX="https://github.com/${REPO}/raw/${BRANCH}/patch/${VERSION}/${SS_NAME}"
+    for F in "${PATCH_FILES[@]}"; do
+      _get_files "${URL_FIX}/${F}" "${WORK_PATH}/patch/${VERSION}/${SS_NAME}/${F}"
+    done
+    ISDL=true
+  fi
 
-  cd "$dir" || exit
-  cp ./"$file" ./"$file"_backup
-  rm ./"$file"
-  wget "$url" -O ./"$file"
-  chmod "$mode" ./"$file"
+  /usr/syno/bin/synopkg stop Virtualization >/dev/null 2>&1
+  sleep 5
+
+  # # 屏蔽认证服务器
+  # if grep -q "synovirtualization.synology.com" /etc/hosts; then
+  #   echo "Already blocked license server: synovirtualization.synology.com."
+  # else
+  #   echo "Add block license server: synovirtualization.synology.com"
+  #   echo "0.0.0.0 synovirtualization.synology.com" | sudo tee -a /etc/hosts
+  # fi
+
+  # 处理 patch 文件
+  SS_PATH="/var/packages/Virtualization/target"
+  _suffix="_backup"
+  for F in "${PATCH_FILES[@]}"; do
+    _process_file "${WORK_PATH}/patch/${VERSION}/${SS_NAME}/${F}" "${SS_PATH}/${F}" "${_suffix}" 0755
+  done
+
+  sleep 5
+  /usr/syno/bin/synopkg start Virtualization >/dev/null 2>&1
+
+  [ "${ISDL}" = true ] && rm -rf "${WORK_PATH:?}/patch/${VERSION}/${SS_NAME}"
 }
 
+uninstall() {
+  _process_file() {
+    local file="${1}" suffix="${2}" mode="${3}"
+    if [ -f "${file}${suffix}" ]; then
+      echo "Restore ${file}"
+      mv -f "${file}${suffix}" "${file}"
+      chown root:root "${file}"
+      chmod "${mode}" "${file}"
+    else
+      echo "Error: Backup file for ${file} does not exist"
+    fi
+  }
 
+  /usr/syno/bin/synopkg stop Virtualization >/dev/null 2>&1
+  sleep 5
 
+  # 处理 patch 文件
+  SS_PATH="/var/packages/Virtualization/target"
+  _suffix="_backup"
+  for F in "${PATCH_FILES[@]}"; do
+    _process_file "${SS_PATH}/${F}" "${_suffix}" 0755
+  done
 
-# 处理每个文件 
-process_file /var/packages/Virtualization/target/usr/lib libsynoccc.so    https://github.com/ohyeah521/vmm_no_limit/raw/main/patch/2.7.0-12229/libsynoccc.so     0755
+  # # 解除屏蔽认证服务器
+  # if grep -q "synovirtualization.synology.com" /etc/hosts; then
+  #   echo "Unblocking license server: synovirtualization.synology.com"
+  #   sudo sed -i '/synovirtualization.synology.com/d' /etc/hosts
+  # else
+  #   echo "License server not blocked: synovirtualization.synology.com."
+  # fi
 
+  sleep 5
+  /usr/syno/bin/synopkg start Virtualization >/dev/null 2>&1
+}
 
+if [ ! "${USER}" = "root" ]; then
+  echo "Please run as root"
+  exit 9
+fi
+
+if [ ! -x "/usr/syno/bin/synopkg" ]; then
+  echo "Please run in Synology system"
+  exit 1
+fi
+
+VERSION="$(/usr/syno/bin/synopkg version Virtualization 2>/dev/null)"
+
+if [ -z "${VERSION}" ]; then
+  # TODO: install ?
+  # /usr/syno/bin/synopkg chkupgradepkg 2>/dev/null
+  # /usr/syno/bin/synopkg install_from_server Virtualization
+
+  echo "Please install Virtual Machine Manager first"
+  exit 1
+fi
+
+ARCH="$(synogetkeyvalue /var/packages/Virtualization/INFO arch)"
+
+SS_NAME="Virtualization-${ARCH}-${VERSION}"
+
+PATCH_FILES=(
+  "lib/libsynoccc.so"
+)
+
+echo "Found ${SS_NAME}"
+
+case "${1}" in
+-r | --uninstall)
+  uninstall
+  ;;
+*)
+  install
+  ;;
+esac
